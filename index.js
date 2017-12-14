ecbe893b1f253dea47ce810b60ba182f35194c53
Devin Moore <devin.moore@volken.org>
2017-12-14 02:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('nested').document.getElementsByClass('target')
+  return document.getElementById('nested').
 }
